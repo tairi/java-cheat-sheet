12bc68d859d1f1b0746eff6d6636e14b9d72c646
Татьяна <34510060+tairi@users.noreply.github.com>
2021-04-20 20:54:36 +0300

    Add files via upload

---
+++ JavaCollections/3102/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.task.task31.task3102;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.TreeSet;
+
+/* 
+Находим все файлы
+*/
+
+public class Solution {
+    public static List<String> getFileTree(String root) throws IOException {
+        List<String> result = new ArrayList<>();
+
+        EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
+        Files.walkFileTree(Paths.get(root), options, 20, new GetFiles(result));
+
+        return result;
+    }
+
+    private static class GetFiles extends SimpleFileVisitor<Path> {
+        private List<String> result;
+
+        public GetFiles(List<String> result) {
+            this.result = result;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+            String s = path.toAbsolutePath().toString();
+            result.add(s);
+            return FileVisitResult.CONTINUE;
+        }
+    }
+}
+/*Находим все файлы
+Реализовать логику метода getFileTree, который должен в директории root найти список всех файлов включая вложенные.
+Используй очередь, рекурсию не используй.
+Верни список всех путей к найденным файлам, путь к директориям возвращать не надо.
+Путь должен быть абсолютный.
+
+
+Требования:
+1. Метод getFileTree должен принимать аргументом String root, по которому нужно найти все вложенные файлы.
+2. Метод getFileTree должен возвращать список строк.
+3. Нужно реализовать метод getFileTree:
+найти все файлы по указанному пути и добавить их в список.
+4. Метод getFileTree должен быть вызван только 1 раз (рекурсию не использовать).*/
